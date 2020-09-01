3f1464aeff7c518ace8e4c2dcac80219bb5587cd
Garin Prakoso <gerin.rin@gmail.com>
2020-09-01 20:11:36 +0700

    update fake token

--- services/login_service/domain/LoginController.go
+++ services/login_service/domain/LoginController.go
@@ -15,7 +15,7 @@ func NewLoginController(client model.MontirClient) *LoginServer {
 	return &LoginServer{NewLoginService(client)}
 }
 
-// Disini adalah pusat Method2 dari Login-Service
+// Disini adalah pusat Method2 dari Login-Service, method2 disini mengacu pada Service login pada file login.proto
 func (c LoginServer) MontirLogin(ctx context.Context, param *model.MontirLoginForm) (*model.LoginResponeMessage, error) {
 	montirAccount := model.MontirAccount{Username: param.Username, Password: param.Password}
 
@@ -23,12 +23,12 @@ func (c LoginServer) MontirLogin(ctx context.Context, param *model.MontirLoginFo
 	log.Print(`password -> `, montirAccount.Password)
 
 	result, err := c.LoginService.MontirLogin(&montirAccount)
+	MontirId := strconv.Itoa(int(result.Id))
 	if err != nil {
-		return nil, err
+		return &model.LoginResponeMessage{Message: MontirId, Token: ""}, err
 	}
 
-	MontirId := strconv.Itoa(int(result.Id))
-	return &model.LoginResponeMessage{Message: MontirId + " Berhasil Login"}, nil
+	return &model.LoginResponeMessage{Message: MontirId, Token: "asdasdqweqweq123123123"}, nil
 }
 
 func (c LoginServer) UserLogin(ctx context.Context, param *model.UserLoginForm) (*model.LoginResponeMessage, error) {
